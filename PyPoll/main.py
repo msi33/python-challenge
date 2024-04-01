00e7d4f5ed2b56ea1e9c4a13373dca6317403151
msi33 <msomowork@gmail.com>
2024-04-01 13:10:47 -0500

    candidate list

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 import csv
 
 # Needed 
-# 1. The total number of votes cast
+# 1. The total number of votes cast - done
 # 2. A complete list of candidates who received votes
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
@@ -13,6 +13,7 @@ import csv
 
 election_file_path = "PyPoll/Resources/election_data.csv" # use relative path
 total_vote = 0
+candidates = []
 # open the file
 
 with open(election_file_path) as election_file:
@@ -20,12 +21,24 @@ with open(election_file_path) as election_file:
     next(csv_file) # skips a row in the file (first row =header row)
     # read a row in the file
     for row in csv_file:
+        #print(row)
         # add to total votes
         total_vote +=1
-        #print(row)
+        #"Ballot ID,County,Candidate"
+        ballot_id = row[0]
+        county = row[1]
+        candidate = row[2]
+        candidates.append(candidate)
+               
 
 # print the result to screen
-print(total_vote)
+print ('Election Results')
+print('-------------------------')
+print(f'Total Votes: {total_vote}')
+print('-------------------------')
+
+print(len(candidates), "Candidates")
+#print(total_vote)
 # print the result to file
 
 
@@ -33,7 +46,6 @@ print(total_vote)
 ### end here
 
 # Example Output
-# Election Results
 # -------------------------
 # Total Votes: 369711
 # -------------------------
