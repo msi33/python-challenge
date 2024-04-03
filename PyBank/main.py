4af3a895b6dc74dc07c1346de9c77d3a99c3830d
msi33 <msomowork@gmail.com>
2024-04-03 11:55:09 -0500

    pasted what is to be done

---
+++ PyBank/main.py
@@ -0,0 +1,23 @@
+
+# Needed 
+# 1. The total number of months included in the dataset
+# 2. The net total amount of "Profit/Losses" over the entire period
+# 3. The changes in "Profit/Losses" over the entire period, and then the average of those changes
+# 4. The greatest increase in profits (date and amount) over the entire period
+# 5. The greatest decrease in profits (date and amount) over the entire period
+
+
+
+
+
+
+
+# example output
+
+# Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $22564198
+# Average Change: $-8311.11
+# Greatest Increase in Profits: Aug-16 ($1862002)
+# Greatest Decrease in Profits: Feb-14 ($-1825558)
