26ffd3c96e8486f3b2febb326209b19a59ec80fa
msi33 <msomowork@gmail.com>
2024-04-12 16:29:39 -0500

    write to file

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,22 +7,9 @@
 # 5. The greatest decrease in profits (date and amount) over the entire period
 
 
-
+### start here
 # # Importing the necessary modules/libraries
 # import csv
-# # create variables
-# # open the file
-# budget_file_path = "Resources/budget_data.csv"
-
-
-
-
-# total_months = 0
-# total_income = 0
-# dates = []
-# profit_loss = []
-# total_profit_loss = 0
-### start here
 import csv
 # create variables
 # open the file
@@ -31,7 +18,6 @@ total_months = 0
 total_income = 0
 dates = []
 profit_loss = []
-total_profit_loss = 0
 
 with open(budget_file_path) as budget_file:
     csv_file = csv.reader(budget_file)
@@ -55,174 +41,46 @@ with open(budget_file_path) as budget_file:
         
         #print(total_income,previous_income, income_change, profit_loss)
         # Total net amount of profit/losses over entire period
-        print(total_profit_loss)
+        print(profit_loss)
 
 
         # Greatest increase in profit
         greatest_increase = max(profit_loss)
         greatest_index = profit_loss.index(greatest_increase)
+
         greatest_date = dates[greatest_index]
-        
         #print(greatest_increase,greatest_date)
 
         # Greatest decrease in profit_losses
         greatest_decrease = min(profit_loss)
         loss_index = profit_loss.index(greatest_decrease)
         loss_date = dates[loss_index]
-        print(greatest_decrease,loss_date)
-
-
-    # Average change in profit/losses between months over entire period
-    average_change = sum(profit_loss)/len(profit_loss)
-    print(average_change)
-    print(profit_loss)
+        # print(greatest_decrease,loss_date)
+        #Average change in profit/losses between months over entire period
+        average_change = sum(profit_loss)/len(profit_loss)
+        # print(profit_loss)
+        # print(len(profit_loss))
         
 # print the results to screen
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${total_income}")
-# print(f"Total Months: {total_months}")
-# print(f"Average Change: $-{average}")
+print(f"Average Change: $-{average_change}")
+print(f"Greate Increase in Profits: {greatest_date}$({greatest_increase})")
+print(f"Greate Decrease in Profits: {loss_date}$({greatest_decrease})")
+
 # print the results to file
 
+out_file_path = "PyBank\budget_results.txt"
+with open(out_file_path,'w') as file_out:
+    file_out.write('Financial Analysis\n')
+    file_out.write("----------------------------\n")
+    file_out.write(f"Total Months: {total_months}\n")
+    file_out.write(f"Total: ${total_income}\n")
+    file_out.write(f"Average Change: $-{average_change}\n")
+    file_out.write(f"Greatest Increase in Profits: {greatest_date}${greatest_increase}\n")
+    file_out.write(f"Greatest Decrease in Profits: {loss_date}${greatest_decrease}\n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# previous_income = 0
-# month_of_change = []
-# income_change = 0
-# income_change_list = []
-# greatest_increase = 0
-# greatest_decrease = 0
-# income_average = 0
-
-
-
-# with open(budget_file_path) as budget_file:
-#     csv_file = csv.reader(budget_file)
-#     next(csv_file) # skips a row in the file (first row =header row)
-#      # read a row in the file
-#     for row in csv_file:
-#         print(row)
-#         # add to total months
-#         total_months+= 1
-#         #Calculate the total income
-#         total_income += int(row[1])
-#     print(row)
-    
-    
-    
-    
-    
-    # print(total_income)
-    #     previous_income = float(row[1])
-    #     income_change =  float(row[1]) - previous_income
-    #     profit.append(income_change)
-        #print(profit)
-
-
-
-      # Calculate the change, then add it to list of changes
-        # income_change = int(row[1])-value
-        # profits.append(change)
-        # value = int(row[1])
-
-
-
-
-
-
-
-
-    
-        # # estimate the average change in incomes between months over the entire period
-         #income_change = float(row[1]) - previous_income
-        # previous_income = float(row[1])
-        # print(previous_income)
-        # #Calculate the average change in revenue between months over the entire period
-        # revenue_change = float(row["Profit/Losses"])- previous_revenue
-        # previous_revenue = float(row["Profit/Losses"])
-        # revenue_change_list = revenue_change_list + [revenue_change]
-        # month_of_change = [month_of_change] + [row["Date"]]
-
-
-    #print(previous_income)
-        # previous_income = float(row[1])
-        # income_change +=  income_change
-        # print(income_change)
-        # month_of_change+= row[0]
-        # income_change_list = income_change_list + [income_change]
-
-    # greatest increase in income over the entire period
-    #    greatest_increase = max (income_change)
-    
-    
-
-# #Greatest increase in profits
-#     greatest_increase = max(profits)
-#     greatest_index = profits.index(greatest_increase)
-#     greatest_date = dates[greatest_index]
-
-#     if income_change > greatest_increase[1]:
-#             greatest_increase[1] = income_change[1]
-#             greatest_increase[0] = row[0]
-#             print(greatest_increase)
-
-#          #The greatest increase in revenue (date and amount) over the entire period
-#         if income_change > greatest_increase[1]:
-#             greatest_increase[1]= income_change
-#             greatest_increase[0] = row['Date']
-
-
-
-#  #The greatest increase in revenue (date and amount) over the entire period
-#         if revenue_change>greatest_increase[1]:
-#             greatest_increase[1]= revenue_change
-#             greatest_increase[0] = row['Date']
-
-
-       # total_amount+=int(row[1])
-      #  average = total_amount/total_months
-
-     
-
-
-
-
-
-
-# print the results to screen
-# print("Financial Analysis")
-# print("----------------------------")
-# print(f"Total: ${total_amount}")
-# print(f"Total Months: {total_months}")
-# print(f"Average Change: $-{average}")
-# print the results to file
-
-
-
-### end here
-
-
-# example output
-
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $22564198
-# Average Change: $-8311.11
-# Greatest Increase in Profits: Aug-16 ($1862002)
-# Greatest Decrease in Profits: Feb-14 ($-1825558)
+# end here
 
